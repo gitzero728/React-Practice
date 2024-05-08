f6bed16c173d1a9c4585d21345f4590797a4d8ab
gitzero728 <happykong728@gmail.com>
2024-05-08 21:44:09 +0900

    deleted unnecessary codes.

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -7,10 +7,6 @@ export default function index() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
         <Link className="App-link" to="/login">
           Press this
         </Link>
