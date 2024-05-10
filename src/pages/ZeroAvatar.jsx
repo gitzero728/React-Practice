3c27a66a81aad7703a0669ad67e9932dbdb6fe0f
gitzero728 <happykong728@gmail.com>
2024-05-10 16:31:37 +0900

    updated gitignore

---
+++ src/pages/ZeroAvatar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ZeroAvatar from "../components/ZeroAvatar/ZeroAvatar";
+
+export default function ZeroAvatarPage() {
+  return (
+    <div>
+      <ZeroAvatar />
+    </div>
+  );
+}
