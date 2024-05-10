37c37ebf5a9f94b101b36bbcc84902f50f268296
gitzero728 <happykong728@gmail.com>
2024-05-10 11:39:15 +0900

    error fixed

--- src/data.js
+++ src/data.js
@@ -1,42 +1,42 @@
 export default [
   {
     title: "1",
-    url: "./assert/1.jpeg",
+    url: "assets/1.jpeg",
     description: "zero 1",
   },
   {
     title: "2",
-    url: "./assert/2.png",
+    url: "assets/2.png",
     description: "zero 2",
   },
   {
     title: "3",
-    url: "./assert/3.png",
+    url: "assets/3.png",
     description: "zero 3",
   },
   {
     title: "4",
-    url: "./assert/4.png",
+    url: "assets/4.png",
     description: "zero 4",
   },
   {
     title: "5",
-    url: "./assert/5.png",
+    url: "assets/5.png",
     description: "zero 5",
   },
   {
     title: "6",
-    url: "./assert/6.png",
+    url: "assets/6.png",
     description: "zero 6",
   },
   {
     title: "7",
-    url: "./assert/7.png",
+    url: "assets/7.png",
     description: "zero 7",
   },
   {
     title: "8",
-    url: "./assert/8.png",
+    url: "assets/8.png",
     description: "zero 8",
   },
 ];
